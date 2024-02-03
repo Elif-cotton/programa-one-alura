f6166da366c1ee50627aae7fef200ef95983efcb
“elif-cotton” <elif.watercolors@gmail.com>
2024-02-03 13:36:35 -0300

    practicar función

--- javascript/logica-programacion-2/app.js
+++ javascript/logica-programacion-2/app.js
@@ -1,9 +1,32 @@
-let titulo = document.querySelector('h1');//objeto
-titulo.innerHTML = 'Juego del número secreto';
+//let titulo = document.querySelector('h1');//objeto
+//titulo.innerHTML = 'Juego del número secreto';
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un número del 1 al 10';
+//let parrafo = document.querySelector('p');
+//parrafo.innerHTML = 'Indica un número del 1 al 10';
 
-function intentoDeUsuario(){
-    alert('Click desde el botón');
+let numeroSecreto = generarNumeroSecreto();
+//console.log(numeroSecreto);
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);//objeto
+    elementoHTML.innerHTML = texto;
+    return;  //buena práctica
 }
+
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(typeof(numeroDeUsuario));
+    console.log(typeof(numeroSecreto));
+    console.log(numeroSecreto);
+    console.log(numeroDeUsuario);
+    console.log(numeroDeUsuario === numeroSecreto);
+
+    return ;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor((Math.random()*10))+1;
+}
+
+asignarTextoElemento('h1', 'Juego del número secreto');
+asignarTextoElemento('p', 'Indica un número del 1 al 10');
