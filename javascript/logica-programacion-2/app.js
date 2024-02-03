8314bb880aaa32546b60970cd600ca2105d948a5
“elif-cotton” <elif.watercolors@gmail.com>
2024-02-03 18:41:23 -0300

    función reiniciarJuego

--- javascript/logica-programacion-2/app.js
+++ javascript/logica-programacion-2/app.js
@@ -1,6 +1,5 @@
-let numeroSecreto = generarNumeroSecreto();
-let intentos = 1;
-console.log(numeroSecreto);
+let numeroSecreto = 0;
+let intentos = 0;
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);//objeto
@@ -14,6 +13,7 @@ function verificarIntento(){
     if(numeroDeUsuario === numeroSecreto){
         asignarTextoElemento('p',`Acertaste en ${intentos} ${(intentos === 1) ? 'intento': 'intentos'}.
         El número secreto era ${numeroSecreto}.`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else{
         //el usuario no acertó
         if (numeroDeUsuario > numeroSecreto){
@@ -29,14 +29,33 @@ function verificarIntento(){
 }
 
 function limpiarCaja() {
-    let valorCaja = document.querySelector('#valorUsuario');
-    valorCaja.value='';
-    
+    document.querySelector('#valorUsuario').value = ''; 
 }
 
 function generarNumeroSecreto() {
     return Math.floor((Math.random()*10))+1;
 }
 
-asignarTextoElemento('h1', 'Juego del número secreto');
-asignarTextoElemento('p', 'Indica un número del 1 al 10');
+function condicionesInciales() {
+    asignarTextoElemento('h1', 'Juego del número secreto');
+    asignarTextoElemento('p', 'Indica un número del 1 al 10');
+    //generar el número aleatorio
+    numeroSecreto = generarNumeroSecreto();
+    //resetear intentos
+    intentos=1;
+
+}
+function reiniciarJuego() {
+    //limpiar la caja
+    limpiarCaja();
+    //indicar mensaje de intervalo de números
+    //generar el número aleatorio
+    //resetear intentos
+    condicionesInciales();
+    //desabilitar el botón de juego nuevo
+    document.getElementById('reiniciar').setAttribute('disabled','true');
+    
+}
+
+condicionesInciales();
+
